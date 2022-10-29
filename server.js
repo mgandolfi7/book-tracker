dfe93279e9ea395f4ea4c4dcd234f2ee74614ed2
Michael <michaelgandolfi7@gmail.com>
2022-10-29 13:16:40 -0500

    installed cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
 const PORT = 8000;
 require("dotenv").config();
 
@@ -18,6 +19,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     db.collection("books").find().toArray()
